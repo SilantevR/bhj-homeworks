e80c8d52b5981fac963f51409ef9b0090eabf03d
SilantevR <craft_89@inbox.ru>
2022-06-04 11:10:44 +0300

    corrections

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -3,12 +3,17 @@ for (let item of links) {
   item.onclick = function () {
     item.closest("li.menu__item");
     let i = item.closest("li.menu__item");
-    if (i.querySelector("ul.menu_sub")) {
-      let active = document.getElementsByClassName("menu_active").item(0);
-      if (active) {
-        active.className = "menu menu_sub";
+    if (i.querySelector("ul.menu_active")) {
+      i.querySelector("ul.menu_active").className = "menu menu_sub";
+      return false;
+    } else if (i.querySelector("ul.menu_sub")) {
+      if (i.closest("ul.menu").querySelector("ul.menu_active")) {
+        i.closest("ul.menu").querySelector("ul.menu_active").className =
+          "menu menu_sub";
+        i.querySelector("ul.menu_sub").className += " menu_active";
+      } else {
+        i.querySelector("ul.menu_sub").className += " menu_active";
       }
-      i.querySelector("ul.menu_sub").className += " menu_active";
       return false;
     }
   };
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,6 +1,58 @@
 const arrows = document.getElementsByClassName("slider__arrow");
 const dots = Array.from(document.getElementsByClassName("slider__dot"));
 const slides = Array.from(document.getElementsByClassName("slider__item"));
+let numOfSlide = 0;
+dots[numOfSlide].className += " slider__dot_active";
+
+let deactivate = function (number) {
+  let active = slides[number];
+  if (active) {
+    active.className = "slider__item";
+  }
+  let dot = dots[number];
+  if (dot) {
+    dot.className = "slider__dot";
+  }
+};
+
+let activate = function (number) {
+  if (number > slides.length - 1) {
+    numOfSlide = 0;
+    slides[numOfSlide].className += " slider__item_active";
+    dots[numOfSlide].className += " slider__dot_active";
+  }
+  if (number < 0) {
+    numOfSlide = slides.length - 1;
+    slides[numOfSlide].className += " slider__item_active";
+    dots[numOfSlide].className += " slider__dot_active";
+  }
+  slides[number].className += " slider__item_active";
+  dots[number].className += " slider__dot_active";
+};
+
+arrows.item(1).onclick = function () {
+  deactivate(numOfSlide);
+  numOfSlide++;
+  activate(numOfSlide);
+};
+
+arrows.item(0).onclick = function () {
+  deactivate(numOfSlide);
+  numOfSlide--;
+  activate(numOfSlide);
+};
+
+for (let item in dots) {
+  dots[item].onclick = function () {
+    deactivate(numOfSlide);
+    numOfSlide = item;
+    activate(numOfSlide);
+  };
+}
+
+/*const arrows = document.getElementsByClassName("slider__arrow");
+const dots = Array.from(document.getElementsByClassName("slider__dot"));
+const slides = Array.from(document.getElementsByClassName("slider__item"));
 
 let numOfSlide = 0;
 dots[numOfSlide].className += " slider__dot_active";
@@ -44,4 +96,4 @@ for (let item in dots) {
     slides[numOfSlide].className += " slider__item_active";
     dots[numOfSlide].className += " slider__dot_active";
   };
-}
+}*/
